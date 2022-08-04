2c3e9b071e08b311c057a004e97a22cd92653598
alikenova312 <alikenova312@icloud.com>
2022-08-04 19:22:11 +0600

    dz4

---
+++ main.js
@@ -0,0 +1,147 @@
+// const a = 'test'
+// if (a === 'test'){
+//     console.log('верно')
+// }else {
+//     console.log('неверно')
+// }
+
+// a === 'test' ? console.log('true') : console.log('false');
+
+// switch (a){
+//     case 'test':
+//         console.log('верно')
+//         break
+//     default:
+//         console.log('неверно')
+// }
+
+
+// let number;
+// data = false;
+//
+// data === true ? number = 2 : number = 3
+//
+// console.log(number)
+
+// let time = prompt('Когда вы собираетесь к нам прийти?')
+//
+// switch (time){
+//     case "23.00":
+//     case "00.00":
+//     case "01.00":
+//     case "02.00":
+//     case "03.00":
+//     case "04.00":
+//     case "05.00":
+//     case "06.00":
+//     case "07.00":
+//     case "08.00":
+//     case "09.00":
+//         alert('в это время мы не работаем')
+//         break;
+//     case "10.00":
+//     case "11.00":
+//     case "12.00":
+//         alert('будем рады вас видеть')
+//         break;
+//     case "13.00":
+//     case "14.00":
+//         alert('у нас обед')
+//         break;
+//     case "15.00":
+//     case "16.00":
+//     case "17.00":
+//     case "18.00":
+//     case "19.00":
+//     case "20.00":
+//     case "21.00":
+//     case "22.00":
+//         alert('будем рады вас видеть')
+// }
+
+
+// let m = 52
+// let n
+// if (m > 50){
+//     n = 'больше'
+// }else {
+//     n = 'меньше'
+// }
+//
+// m > 50 ? n = 'больше' : n = 'меньше'
+//
+// console.log(m)
+
+// let a = 5;
+// if (a === 0 || a === 2 ){
+//     console.log(a + 7)
+// } else {
+//     console.log(a / 10)
+// }
+// console.log(a)
+
+
+// let person = confirm('вам есть 18?')
+// if (person){
+//     console.log('заходите')
+// }else {
+//     console.log('уходите')
+// }
+
+
+// let a = 1
+// let b = 6
+//
+// if (a <= 1 && b >= 6){
+//     console.log(a + b)
+// }else {
+//     console.log(a - b)
+// }
+
+
+// const pasha = 17;
+// if (pasha <= 17){
+//     console.log('ты еще ребенок')
+// }else {
+//     console.log('ты уже взрослый')
+// }
+
+// const seasons = 'december'
+// switch (seasons){
+//     case 'december':
+//     case 'feb':
+//     case 'jen':
+//         console.log('winter')
+//         break
+//     case 'mar':
+//     case 'apr':
+//     case 'may':
+//         console.log('spring')
+//         break
+//     case 'jul':
+//     case 'july':
+//     case 'august':
+//         console.log('summer')
+//         break
+//     case 'sep':
+//     case 'nov':
+//     case 'okt':
+//         console.log('autumn')
+//         break
+// }
+
+
+// let time = 15
+// if (time >= 0 && time<= 15){
+//     console.log('время относится к четверти часа')
+// }else if (time >= 15 && time <= 30){
+//     console.log('время относится к половине часа')
+// }else if (time >= 30 && time <= 45){
+//     console.log('время относится к 3/4 часа')
+// }else if (time >= 45 && time <= 60){
+//     console.log('время относится к целому часу')
+// }else {
+//     console.log('неизвестный диапазон')
+// }
+
+
